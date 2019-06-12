f0dc30db0a679e1b2e71634125e67789a50d3e57
Phillip Rhinehart <phillip.rhinehart@icidigital.com>
2019-06-12 11:22:11 -0400

    changes to insertionSort.ts

--- insertion_sort.ts
+++ insertion_sort.ts
@@ -5,15 +5,13 @@ const insertionSort = (list: number[]): number[] => {
     for (let i: number = 1; i < list.length; ++i) {
         let k: number = i;
         let j: number = k - 1;
-        while (j >= 0) {
+        while (j >= 0 && list[k] < list[j]) {
             // if the ith + 1 element > ith swap them
-            if (list[k] < list[j]) {
-                let smaller: number = list[k];
-                list[k] = list[j];
-                list[j] = smaller;
-                --j;
-                --k;
-            } else { break; }
+            let smaller: number = list[k];
+            list[k] = list[j];
+            list[j] = smaller;
+            --j;
+            --k;
         }
     }
     return list;
