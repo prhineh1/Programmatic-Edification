b9bdbdf75f128454324eb09d7875ecfb1cf14b47
Phillip Rhinehart <phillip.rhinehart@icidigital.com>
2019-06-14 16:34:00 -0400

    linked list cont

--- linked-list/linked_list.js
+++ linked-list/linked_list.js
@@ -1,5 +1,31 @@
-import Node from '../node/node.js';
-
 const LinkedList = (...args) => {
-
+    let props = {
+        head: {
+            value: null,
+            writable: true,
+            configurable: false,
+            enumerable: true
+        },
+        length: {
+            get() {
+                let len = 1;
+                let node = this.head;
+                while (node.next !== null) {
+                    node = node.next;
+                    ++len;
+                }
+                return len;
+            },
+            writable: false,
+            configurable: false,
+            enumerable: true
+        }
+    }
+    for (let i = 0; i < args.length - 1; i ++) {
+        if (i < 1) {
+            props.head = args[i];
+        }
+        args.next = args[i+1];
+    }
+    Object.assign({}, props);
 }
--- node/node.js
+++ node/node.js
@@ -1,15 +1,32 @@
 'use strict';
 
 const Node = (...args) => {
+    if (args.length < 1) { return; }
+
     let col = [];
-    for (let i=0; i<args.length; ++i) {
-        col[i] = {
-            data: args[i],
-            next: null,
-            prev: null
+    for (let i = 0; i < args.length; ++i) {
+        const props = {
+            data: {
+                value: args[i],
+                writable: true,
+                configurable: false,
+                enumberable: true
+            },
+            next: {
+                value: null,
+                writable: true,
+                configurable: false,
+                enumberable: true
+            },
+            prev: {
+                value: null,
+                writable: true,
+                configurable: false,
+                enumberable: true
+            }
         }
+        col[i] = Object.create({}, props);
     }
-    return col;
+    return col.length < 2 ? col[0] : col;
 };
-console.log(...Node(1,2,3));
 module.exports = Node;
