1e63802323c07e39a488aa60ccedf72076d15c0e
Phillip Rhinehart <phillip.rhinehart@icidigital.com>
2019-06-20 16:10:43 -0400

    bin-tree traversals

--- binary-tree/binary_tree.js
+++ binary-tree/binary_tree.js
@@ -2,31 +2,77 @@ var BinTreeNode = require('./binary_tree_node');
 
 class BinaryTree {
     constructor(head) {
-        this.head = new BinTreeNode(head);
+        if (head) {
+            this.head = new BinTreeNode(head);
+        } else {
+            this.head = null;
+        }
     }
+
+    get height() {
+        return this.treeHeight(this.head)
+    }
+
+    treeHeight(node = this.head) {
+        if (!this.head) {
+            return 0;
+        }
+
+        const left = node.lchild ? this.treeHeight(node.lchild): 0;
+        const right = node.rchild ? this.treeHeight(node.rchild): 0;
+        return 1 + Math.max(left,right);
+    }
+
     lappend(data, node) {
         if (node) {
             node.lchild = new BinTreeNode(data);
             return this.head;
         }
     }
+
     rappend(data, node) {
         if (node) {
             node.rchild = new BinTreeNode(data);
             return this.head;
         }
     }
-    preorderTraversal = function*(node = this.head) {
+
+    preorderTraversal(node = this.head) {
+        let list = [];
+        list = list.concat(node.data);
+        const left = node.lchild ? this.preorderTraversal(node.lchild) : [];
+        const right = node.rchild ? this.preorderTraversal(node.rchild): [];
+        return list.concat(left, right);
+    }
+
+    inorderTraversal(node = this.head) {
+        let list = [];
+        const left = node.lchild ? this.inorderTraversal(node.lchild) : [];
+        list = list.concat(node.data);
+        const right = node.rchild ? this.inorderTraversal(node.rchild) : [];
+        return left.concat(list, right);
+    }
+
+    *postorderTraversal(node = this.head) {
+        if (node.lchild) { yield* this.postorderTraversal(node.lchild); }
+        if (node.rchild) { yield* this.postorderTraversal(node.rchild); }
         yield node.data;
-        if (node.lchild) { yield* this.preorderTraversal(node.lchild); }
-        if (node.rchild) { yield* this.preorderTraversal(node.rchild); }
+        return;
+    }
+
+    *levelorderTraversal(queue = [this.head]) {
+        if (queue[queue.length-1]) {
+            const popped = queue.pop();
+            yield popped.data;
+            queue.unshift(popped.rchild, popped.lchild);
+            yield* this.levelorderTraversal(queue);
+        }
+        if (queue.length) {
+            queue.pop();
+            yield* this.levelorderTraversal(queue);
+        }
         return;
     }
 }
-var x = new BinaryTree(1);
-x.lappend(2, x.head);
-x.rappend(3, x.head.lchild);
-x.lappend(4, x.head.lchild);
-x.rappend(5, x.head);
-x.preorderTraversal();
+
 module.exports = BinaryTree;
--- test/binary_tree.test.js
+++ test/binary_tree.test.js
@@ -7,7 +7,7 @@ describe('binary tree', function() {
     });
 
     it('creates a tree with a null head', function() {
-        assert.deepEqual(new func().head, { data: null, lchild: null, rchild: null });
+        assert.equal(new func().head, null);
     });
 
     it('appends an lchild to the tree', function() {
@@ -23,7 +23,64 @@ describe('binary tree', function() {
     it('preorder traversal', function() {
         const tree = new func(1);
         tree.lappend(2, tree.head);
+        tree.lappend(3, tree.head.lchild);
+        tree.rappend(4, tree.head.lchild);
+        tree.rappend(5, tree.head);
+        const gen = tree.preorderTraversal();
+        assert.deepEqual(gen, [1,2,3,4,5]);
+    });
+
+    it('inorder traversal', function() {
+        const tree = new func(4);
+        tree.lappend(2, tree.head);
+        tree.lappend(1, tree.head.lchild);
+        tree.rappend(3, tree.head.lchild);
+        tree.rappend(5, tree.head);
+        const gen = tree.inorderTraversal();
+        assert.deepEqual(gen, [1,2,3,4,5]);
+    });
+
+    it('postorder traversal', function() {
+        const tree = new func(5);
+        tree.lappend(3, tree.head);
+        tree.lappend(1, tree.head.lchild);
+        tree.rappend(2, tree.head.lchild);
+        tree.rappend(4, tree.head);
+        const gen = tree.postorderTraversal();
+        for (let i = 1; i < 6; ++i) {
+            assert.equal(gen.next().value, i);
+        }
+    });
+
+    it('levelorder traversal', function() {
+        const tree = new func(1);
+        tree.lappend(2, tree.head);
+        tree.lappend(4, tree.head.lchild);
+        tree.rappend(5, tree.head.lchild);
         tree.rappend(3, tree.head);
-        assert.deepEqual(tree, { data: 2, lchild: null, rchild: null });
+        const gen = tree.levelorderTraversal();
+        for (let i = 1; i < 6; ++i) {
+            assert.equal(gen.next().value, i);
+        }
+    });
+
+    it('has a height of 0', function() {
+        const tree = new func();
+        assert.equal(tree.height, 0);
+    });
+
+    it('has a height of 1', function() {
+        const tree = new func(1);
+        assert.equal(tree.height, 1);
+    });
+
+    it('has a height of 3', function() {
+        const tree = new func(1);
+        tree.lappend(2, tree.head);
+        tree.lappend(4, tree.head.lchild);
+        tree.rappend(5, tree.head.lchild);
+        tree.rappend(3, tree.head);
+        tree.rappend(6, tree.head.lchild.lchild);
+        assert.equal(tree.height, 4);
     });
 })
