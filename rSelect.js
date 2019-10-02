f94409dc69613e543d3b05ce3fcf3c4aff9dc22d
Phillip Rhinehart <phillip.rhinehart@icidigital.com>
2019-10-02 12:04:01 -0400

    rSelect

---
+++ rSelect.js
@@ -0,0 +1,29 @@
+'use strict';
+const partition = require ('./rSelect_partition');
+
+const rSelect = (arr, ord) => {
+    if (ord > arr.length) {
+        return null;
+    }
+
+    if (arr.length === 1) {
+        return arr[0];
+    }
+
+    // choose pivot randomly
+    const position = partition(arr, 0, Math.floor(Math.random() * arr.length));
+
+    if (position === ord) {
+        return arr[position];
+    }
+
+    if (position > ord) {
+        return rSelect(arr.slice(0, position), ord);
+    }
+
+    if (position < ord) {
+        return rSelect(arr.slice(position+1), ord - (position+1));
+    }
+}
+
+console.log(rSelect([6,8,9,2,3,22], 1));
---
+++ rSelect_partition.js
@@ -0,0 +1,31 @@
+const partition = (arr, lbound, rbound) => {
+    // i iterates through the array
+    // j tracks where the pivot is placed
+    let i, j;
+    i = j = lbound;
+
+    // ensure rbound is the last element
+    let last;
+    last = arr[arr.length-1];
+    arr[arr.length-1] = arr[rbound];
+    arr[rbound] = last;
+
+    for (i; i < arr.length; ++i) {
+        let temp;
+        if (arr[i] < arr[arr.length-1]) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            ++j;
+        }
+    }
+
+    let swap;
+    swap = arr[arr.length-1];
+    arr[arr.length-1] = arr[j];
+    arr[j] = swap;
+
+    return j;
+}
+
+module.exports = partition;
