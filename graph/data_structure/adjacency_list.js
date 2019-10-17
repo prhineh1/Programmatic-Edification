bba044ffc0af707d7af4ab8681946fd5ea4ea390
Phillip Rhinehart <phillip.rhinehart@icidigital.com>
2019-10-17 15:13:58 -0400

    adjacency_list contd

--- graph/data_structure/adjacency_list.js
+++ graph/data_structure/adjacency_list.js
@@ -4,6 +4,16 @@
 
 const UndirectedGraph = (...args) => {
     const vertices = args.map(arg => {
-
-    })
+        if (/n/.test(arg)) {
+            return {
+                data: arg,
+                edges: []
+            };
+        }
+    });
+    const edges = args.map(arg => {
+        if (/m/.test(arg)) {
+            return arg.split('m');
+        }
+    });
 }
